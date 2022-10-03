5495c869e41c6d96ff0aa7702dfc92d47a6ec550
Evgeniy Sharychenkov <sanctio@yandex.ru>
2022-10-03 13:06:39 +0300

    first commit

--- src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
+++ src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
@@ -1,15 +1,34 @@
 package com.epam.rd.autotasks;
 
 public class DecrementingCarousel {
-    public DecrementingCarousel(int capacity) {
+    private final int CAPACITY;
+    static int[] carousel;
+    int index;
+    boolean isRun;
 
+    {
+        index = 0;
+        isRun = false;
+    }
+    public DecrementingCarousel(int capacity) {
+        this.CAPACITY = capacity;
+        carousel = new int[capacity];
     }
 
     public boolean addElement(int element){
-        throw new UnsupportedOperationException();
+        if (element > 0 && index < CAPACITY && !isRun) {
+            carousel[index++] = element;
+            return true;
+        }
+        return false;
+
     }
 
     public CarouselRun run(){
-       throw new UnsupportedOperationException();
+        if (!isRun) {
+            isRun = true;
+            return new CarouselRun();
+        }
+        return null;
     }
 }
